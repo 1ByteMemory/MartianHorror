aa63467683bd0716f357e9a891d03193f442f174
Dan J <dan.jones8902@gmail.com>
2020-10-22 14:33:48 +0100

    Started work on creating rooms for random room gen

---
+++ Assets/Scripts/Connectors.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connectors : MonoBehaviour
+{
+    public Mesh gizmoMesh;
+    public Transform[] connectorList;
+
+
+    public Transform GetPointWorldPos(int index)
+	{
+        return connectorList[index];
+	}
+
+
+	private void OnDrawGizmos()
+	{
+		for (int i = 0; i < connectorList.Length; i++)
+		{
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(connectorList[i].position, 0.25f);
+
+            Vector3 pos = connectorList[i].position + (connectorList[i].forward * 0.5f);
+            Vector3 size = new Vector3(0.1f, 0.1f, 1);
+
+            if (gizmoMesh != null)
+                Gizmos.DrawMesh(gizmoMesh, pos, Quaternion.LookRotation(connectorList[i].forward), size);
+
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
